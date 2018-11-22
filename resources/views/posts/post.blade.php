f1fd1cf6c645b63c9afbc9023653aafc9bda3811
Juan Gabriel Hernandez <juangahoo@gmail.com>
2018-11-22 16:35:04 -0300

    El Último Commit de la MUERTE

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -2,7 +2,7 @@
     <h4 style="background-color:#808080B3">
         <a href="/posts/{{ $post->id }}" style="text-decoration:none; color:black">{{ $post->title }}</a>
     </h4>
-    <a href="/posts/{{ $post->id }}" data-largesrc="/images/1.jpg" data-title="{{ $post->title }}" data-description= "{{ $post->body }}">
-        <img class="img-responsive" src="/images/thumbs/1.jpg" alt="img01"/>
+    <a href="/posts/{{ $post->id }}" data-largesrc="/images/Shadow-Tomb-Raider-PC-portada.jpg" data-title="{{ $post->title }}" data-description= "{{ $post->body }}">
+        <img class="img-responsive" src="/images/thumbs/Shadow-Tomb-Raider-PC-portada.jpg" alt="img01"/>
     </a>
 </li>
